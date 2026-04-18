3c57161652c0e5efa74ce6c87fbcd8919f89a899
tzangms <tzangms@gmail.com>
2026-04-18 20:30:22 +0800

    chore: scaffold PlatformIO project with LCD hello world

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include "TFT_eSPI.h"
+
+TFT_eSPI tft;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LCD_BACKLIGHT, OUTPUT);
+  digitalWrite(LCD_BACKLIGHT, HIGH);
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(20, 100);
+  tft.print("Claude Buddy boot OK");
+}
+
+void loop() {
+  Serial.println("alive");
+  delay(1000);
+}
